58a96726d704eb3767feeac794bf0783f4237122
JC Mac04 <jcruz21+mac04@gmail.com>
2021-10-03 21:26:22 -0600

    fix models and other ps4 errors  Changes to be committed: 	modified:   .gitignore 	renamed:    app/Http/Controllers/DisplayPhotoControler.php -> app/Http/Controllers/DisplayPhotoController.php 	new file:   app/Models/FileVault.php 	modified:   app/Models/FileVaultModel.php 	modified:   tests/Feature/ThumbnailImagesTest.php

--- tests/Feature/ThumbnailImagesTest.php
+++ tests/Feature/ThumbnailImagesTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ThumbnailImageTest extends TestCase
+class ThumbnailImagesTest extends TestCase
 {
     /**
      * A basic feature test example.
